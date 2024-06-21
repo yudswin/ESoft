0b8f590514f6958b5f5cbdcaa96b07f3c0794580
Duy Nguyễn <93112625+yudswin@users.noreply.github.com>
2024-06-21 23:00:44 +0700

    🚀 Init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+
+  return (
+    <div className="w-screen h-screen bg-violet-400">
+      <div className="w-full h-full flex flex-row justify-center items-center">
+        HELLO EXTENSION
+      </div>
+    </div>
+  )
+}
+
